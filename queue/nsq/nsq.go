f5c1a469c848fe985d22ab4c9adbd8c8b077a3e4
Po-Ying Chen <poying.me@gmail.com>
2017-10-08 16:36:51 +0800

    add nsq support

---
+++ queue/nsq/nsq.go
@@ -0,0 +1,33 @@
+package nsq
+
+import (
+	gonsq "github.com/nsqio/go-nsq"
+	"github.com/poying/goevents"
+)
+
+// NewBus creates an event bus
+func NewBus(producer *gonsq.Producer, consumer *gonsq.Consumer) *goevents.Bus {
+	return goevents.NewBus(producer, &Consumer{consumer: consumer})
+}
+
+// Consumer represents a message consumer
+type Consumer struct {
+	consumer *gonsq.Consumer
+}
+
+// AddHandler adds a message handler to consumer
+func (consumer *Consumer) AddHandler(handler goevents.MessageHandler) {
+	consumer.consumer.AddHandler(gonsq.HandlerFunc(func(nsqMessage *gonsq.Message) error {
+		return handler.HandleMessage(&Message{message: nsqMessage})
+	}))
+}
+
+// Message represents a message
+type Message struct {
+	message *gonsq.Message
+}
+
+// Decode the message
+func (message *Message) Decode() ([]byte, error) {
+	return message.message.Body, nil
+}
